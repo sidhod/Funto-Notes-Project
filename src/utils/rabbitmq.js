e74e353e8a91573c27054c095a5e35066c03589e
unknown <sidhodhank2018@gmail.com>
2022-11-30 11:44:42 +0530

    [Sidhodhan]Added:Reciver function to recive message from rabbitmq

--- src/utils/rabbitmq.js
+++ src/utils/rabbitmq.js
@@ -25,5 +25,31 @@ const sender = () => {
         }, 500);
     });
 }
+const reciver = () => {
+    amqp.connect('amqp://localhost', function (error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function (error1, channel) {
+            if (error1) {
+                throw error1;
+            }
 
+            var queue = 'hello';
+
+            channel.assertQueue(queue, {
+                durable: false
+            });
+
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+            channel.consume(queue, function (msg) {
+                console.log(" [x] Received %s", msg.content.toString());
+            }, {
+                noAck: true
+            });
+        });
+    });
+}
 sender();
+reciver();
